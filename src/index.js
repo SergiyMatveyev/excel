d89649302b01b53544ce6bfaa79217525ff1ecd0
Sergiy Matveyev <centr@centrs-MacBook-Pro.local>
2022-11-20 12:04:15 +0200

    Creared framework

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-
+import {Excel} from '@/components/excel/Excel';
+import {Header} from '@/components/header/Header';
+import {Formula} from '@/components/formula/Formula';
+import {Table} from '@/components/table/Table';
+import {Toolbar} from '@/components/toolbar/Toolbar';
 import './scss/index.scss';
 
-console.log('Working');
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table],
+});
+excel.render();
